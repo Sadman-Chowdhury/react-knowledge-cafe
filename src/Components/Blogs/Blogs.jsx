3e553abdc00a3ce5f033a5216c58cfbdeb578e07
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-03-12 21:00:47 +0600

    Blogs added

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setblogs] = useState([])
 
     useEffect(()=>{
-        fetch('knowledge.json')
+        fetch('knowlwdge.json')
         .then(res => res.json())
         .then(data => setblogs(data))
     },[])
     return (
-        <div>
-            
+        <div className="md: w-2/3">
+            <h1>Blogs</h1>
+            {
+                blogs.map(blog => <Blog blog={blog} key={blog.id}></Blog>)
+            }
         </div>
     );
 };
